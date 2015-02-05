fd1a89c95d78fcfe7d085db3e973df3cccd9c694
cj1m <cj1mckay@gmail.com>
2015-02-05 21:05:09 +0000

    Fixed vertical spacing

--- js/homejs.js
+++ js/homejs.js
@@ -1,11 +1,15 @@
 var beginButton;
 var achievementButton;
+var context;
 window.onload = function(){
   beginButton = document.getElementById('beginbutton');
   achievementButton = document.getElementById('achievementsbutton');
+  var canvas = document.getElementById("animations");
+  context = canvas.getContext("2d");
 
   beginButton.style.cursor = 'pointer';
   achievementButton.style.cursor = 'pointer';
+  context.fillRect(0,0,canvas.width,canvas.height);
 
   beginButton.onclick = function() {
     window.location.href = "begin.html";
