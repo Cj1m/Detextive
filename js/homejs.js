a148df1e4a3af1d00df3f1161c3de6e82b241671
cj1m <cj1mckay@gmail.com>
2015-02-06 22:16:45 +0000

    Animation on fleek

--- js/homejs.js
+++ js/homejs.js
@@ -1,17 +1,24 @@
 var beginButton;
 var achievementButton;
+var canvas;
 var context;
 var cursorSprite;
+
+var objectsOnCanvas = [];
+var objectXs = [];
+
 window.onload = function(){
   beginButton = document.getElementById('beginbutton');
   achievementButton = document.getElementById('achievementsbutton');
-  var canvas = document.getElementById("animations");
+  canvas = document.getElementById("animations");
   cursorSprite = document.getElementById("cursorsprite");
   context = canvas.getContext("2d");
 
   beginButton.style.cursor = 'pointer';
   achievementButton.style.cursor = 'pointer';
-  context.fillRect(0,0,canvas.width,canvas.height);
+  canvas.width = window.innerWidth;
+  canvas.height = 250;
+
 
   beginButton.onclick = function() {
     window.location.href = "begin.html";
@@ -22,6 +29,9 @@ window.onload = function(){
   };
 
   setInterval(blinkCursorSprite, 530);
+  setInterval(draw, 33);
+  addObject();
+  setInterval(addObject, 6000);
 }
 
 function blinkCursorSprite(){
@@ -31,3 +41,38 @@ function blinkCursorSprite(){
     cursorSprite.style.opacity = 0;
   }
 }
+
+function draw(){
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  for(i = 0; i < objectsOnCanvas.length; i++){
+    var img = new Image();
+    img.src = objectsOnCanvas[i];
+    context.drawImage(img, objectXs[i],canvas.height-img.height, img.width, img.height);
+    objectXs[i]++;
+    if(objectXs[i] > canvas.width){
+      objectsOnCanvas.splice(i, 1);
+      objectXs.splice(i, 1);
+    }
+    console.log(objectsOnCanvas);
+  }
+}
+
+function addObject(){
+  var x = 1 + Math.round(Math.random() * 6);
+  var objectImage ="assets/object" + x + ".png";
+
+
+  objectsOnCanvas.push(objectImage);
+  objectXs.push(0);
+}
+
+(function() {
+        window.addEventListener('resize', resizeCanvas, false);
+
+        function resizeCanvas() {
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+        }
+        resizeCanvas();
+})();
