34e4ba26bb41d9a6456e1d9ef1681d0d9f59be47
Chris J. Perceval-Maxwell <chris.perceval-maxwell@hotmail.com>
2015-03-01 21:29:25 +0000

    Fixed text printing - didn't commit last time

--- js/gamejs.js
+++ js/gamejs.js
@@ -35,11 +35,11 @@ function addTextToScreen(textForScreen){
   textOnScreen = textOnScreen + textForScreen;
 }
 type();
-addTextToScreen("is it a bird?");
+/*addTextToScreen("is it a bird?");
 addTextToScreen("<br>is it a plane?");
 addTextToScreen("<br>...");
-addTextToScreen("<br>no - it's <i>superman</i>");
-
+addTextToScreen("<br>no - it's <i>superman</i>");*/
+addTextToScreen("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
 
 
 
