7e2db7ef19b08b14e29ced27d74f4ad8011c8f6b
Chris J. Perceval-Maxwell <chris.perceval-maxwell@hotmail.com>
2015-03-01 21:50:14 +0000

    Changed position of input box

--- js/gamejs.js
+++ js/gamejs.js
@@ -1,7 +1,7 @@
 var timer;
 var timesRun = 0;
 var textOnScreen = "";
-var typeTime = 45; /*Very fast for immediate results, good reading speed is 45*/
+var typeTime = 5; /*Very fast for immediate results, good reading speed is 45*/
 //var i = 0;
 //var isTag = false;
 window.onLoad = (function($) {
@@ -40,8 +40,6 @@ addTextToScreen("<br>is it a plane?");
 addTextToScreen("<br>...");
 addTextToScreen("<br>no - it's <i>superman</i>");*/
 addTextToScreen("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
-
-
 
 $(window).resize(fixSizes);
 
