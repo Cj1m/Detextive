e114c7168178e081f6fc24f887be428f1987b970
cj1m <cj1mckay@gmail.com>
2015-03-02 19:37:49 +0000

    fixed text overflow

--- js/gamejs.js
+++ js/gamejs.js
@@ -39,7 +39,7 @@ type();
 addTextToScreen("<br>is it a plane?");
 addTextToScreen("<br>...");
 addTextToScreen("<br>no - it's <i>superman</i>");*/
-addTextToScreen("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
+//addTextToScreen("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
 
 
 
@@ -60,11 +60,10 @@ function fixSizes(){
 
 function printText(event) {
   if(event.keyCode == 13){
-      printText = document.getElementById("inputText").value;
-      addTextToScreen("<br>" + printText);
+      printText = document.getElementById("inputText").value.toString();
+      addTextToScreen("<br>" + printText.toString());
       x = document.getElementById("inputText");
       x.value = "";
-
   }
 }
 
