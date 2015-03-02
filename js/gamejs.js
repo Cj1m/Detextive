61babbf32d24e22b5d299f14c3facde5d0c3ca61
cj1m <cj1mckay@gmail.com>
2015-03-02 21:49:24 +0000

    notes work!

--- js/gamejs.js
+++ js/gamejs.js
@@ -7,6 +7,7 @@ var typeTime = 5; /*Very fast for immediate results, good reading speed is 45*/
 window.onLoad = (function($) {
   timer = setInterval(fixSizes, 1);
   $("#inputTextMain").keypress(printText);
+  $("#inputTextNotes").keypress(printNotes);
 })(jQuery);
 
 var i = 0,
@@ -25,7 +26,7 @@ function type() {
     elem.innerHTML = text;
 
     var char = text.slice(-1);
-    console.log(char);
+
     if( char === '<' ) isTag = true;
     if( char === '>' ) isTag = false;
     elem.scrollTop = elem.scrollHeight;
@@ -37,10 +38,10 @@ function type() {
 function addTextToScreen(textForScreen){
   textOnScreen = textOnScreen + textForScreen;
 }
-type();
+
 //**this will look nice when auto scroll is fixed** --> addTextToScreen('<h2 style="font-family:neb">Detetxtive...               It is time to begin your story                          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></h2>');
 addTextToScreen('<p style="font-family:ebitparty">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>');
-
+type();
 $(window).resize(fixSizes);
 
 function fixSizes(){
@@ -65,4 +66,16 @@ function printText(event) {
   }
 }
 
-//when printing notes add a bullet point
+function printNotes(event) {
+  if(event.keyCode == 13){
+    var x = document.getElementById("inputTextNotes");
+      if(x.value != ""){
+        var printNotesText = document.getElementById("inputTextNotes").value;
+        var inputText = "&bull;" + printNotesText + "<br />";
+        var elem = document.getElementById('notesText');
+        elem.innerHTML += inputText;
+        elem.scrollTop = elem.scrollHeight;
+        x.value = "";
+      }
+  }
+}
