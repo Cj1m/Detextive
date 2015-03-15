a0b2d85531b1db1431b333ca0f64ec9e554e29bf
cj1m <cj1mckay@gmail.com>
2015-03-15 21:11:58 +0000

    added clearText()

--- js/gamejs.js
+++ js/gamejs.js
@@ -39,7 +39,7 @@ function addTextToScreen(textForScreen){
   textOnScreen = textOnScreen + textForScreen;
 }
 
-addTextToScreen('<h2 id="title" style="font-family:neb">Detetxtive...               It is time to begin your story                                     </h2>');
+addTextToScreen('<h2 id="title" style="font-family:neb">Detetxtive...               It is time to begin your story</h2>');
 addTextToScreen('<p style="font-family:ebitparty">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>');
 type();
 $(window).resize(fixSizes);
@@ -78,4 +78,9 @@ function printNotes(event) {
         x.value = "";
       }
   }
+}
+
+function clearText(){
+  var elem = document.getElementById('paraText');
+  elem.value = "";
 }
